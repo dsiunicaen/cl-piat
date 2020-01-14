c2c85d1b1ee5fd5416788c06798ae6e1733e20b0
Pet Crocker <peter@cumulusnetworks.com>
2020-01-14 08:28:53 +0000

    changes Vagrant

--- cldemo2/start-demo.sh
+++ cldemo2/start-demo.sh
@@ -18,7 +18,7 @@ if [ "$1" != "" ]; then
             vagrant up leaf01 leaf02 leaf03 leaf04 spine01 spine02 spine03 spine04 server01 server02 server03 server04 server05 server06 server07 server08 server09 server10 service01 service02 fw1 fw2
             ;;
         piat)
-            vagrant up leaf01 leaf02 leaf03 leaf04 spine01 spine02 spine03 spine04 server01 server02 server03 server04 server05 server06 server07 server08 server09 server10 service01 service02 fw1 fw2 super01 super02 super03 super04 exit01 exit02 rtr01 rtr02
+            vagrant up leaf01 leaf02 leaf03 leaf04 spine01 spine02 spine03 spine04 server01 server02 server03 server04 server05 server06 server07 server08 server09 server10 service01 service02 super01 super02 super03 super04 exit01 exit02 rtr01 rtr02
             ;;
         full)
             vagrant up
